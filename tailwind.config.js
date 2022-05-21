c65f3b66fc3cd65281b0a3773a7bc25c1d6f1c21
BenBoisclair <104126246+BenBoisclair@users.noreply.github.com>
2022-05-21 15:06:33 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'secondary': '#18BFB4',
+        'body': '#142134' 
+      },
+      fontFamily: {
+        'roboto': ["'Roboto'",'sans-serif']
+      }
+    },
+  },
+}
